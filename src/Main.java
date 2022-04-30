954686c494aa9ea38545bd9ef591e2f8f27397a1
Diogo <diogoramiresferrer@gmail.com>
2022-05-01 00:09:29 +0200

    Big improvements

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import GUI.MainMenu;
-
-public class Main {
-
-    public static void main(String[] args) {
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.setVisible(true);
-    }
-}
